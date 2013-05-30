9fdf024bbf6b4be90f713e35a237011f59296a88
lulalala <mark@goodlife.tw>
2013-05-30 20:19:01 +0800

    Refactor: extract path computing logic into a method

--- lib/multi_logger.rb
+++ lib/multi_logger.rb
@@ -4,17 +4,8 @@ module MultiLogger
   class << self
     def add_logger(name, path=nil)
       name = name.to_s
-      if path.nil?
-        path = name.underscore
-      end
-      if !path.include?('/')
-        path = Rails.root.join('log',path).to_s
-      end
-      if !path.end_with?('.log')
-        path += '.log'
-      end
 
-      logger = Logger.new(path)
+      logger = Logger.new(get_path(name, path))
 
       if self.respond_to? name
         raise "'#{name}' is reserved and can not be used as a log name."
@@ -24,6 +15,19 @@ module MultiLogger
         define_method name.to_sym do
           logger
         end
+      end
+    end
+
+    # Computes log file path
+    def get_path(name, path=nil)
+      if path.nil?
+        path = name.underscore
+      end
+      if !path.include?('/')
+        path = Rails.root.join('log',path).to_s
+      end
+      if !path.end_with?('.log')
+        path += '.log'
       end
     end
   end

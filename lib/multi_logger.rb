04bc24f34689c2d0f6bf18558ada141983abe8c4
Keisuke KITA <kei.kita2501@gmail.com>
2013-09-03 08:35:46 +0900

    accept shift_age and shift_size

--- lib/multi_logger.rb
+++ lib/multi_logger.rb
@@ -9,7 +9,7 @@ module MultiLogger
       if rails_logger_class.method_defined?(name)
         raise "'#{name}' is reserved in #{rails_logger_class} and can not be used as a log accessor name."
       else
-        logger = Logger.new(get_path(name, options[:path]))
+        logger = Logger.new(*extract_options(name, options))
         rails_logger_class.class_eval do
           define_method name.to_sym do
             logger
@@ -27,12 +27,13 @@ module MultiLogger
       if path.nil?
         path = name.underscore
       end
-      if !path.include?('/')
+      unless path.include?('/')
         path = Rails.root.join('log',path).to_s
       end
-      if !path.end_with?('.log')
+      unless path.end_with?('.log')
         path += '.log'
       end
+      path
     end
 
     def get_rails_logger_class
@@ -42,6 +43,17 @@ module MultiLogger
         ActiveSupport::Logger
       else
         raise 'Rails logger not found'
+      end
+    end
+
+    def extract_options(name, options)
+      if options[:shift_age] && options[:shift_size]
+        [get_path(name, options[:path]), options[:shift_age], options[:shift_size]]
+      elsif options[:shift_age]
+        # options[:shift_age] => 'daily', 'weekly'
+        [get_path(name, options[:path]), options[:shift_age]]
+      else
+        [get_path(name, options[:path])]
       end
     end
   end

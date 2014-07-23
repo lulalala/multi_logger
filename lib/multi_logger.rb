8f28a6e0b65eb239413c7cd95a3e3ae9ad918312
Karuppasamy M <karuppasamy.m@cognizant.com>
2014-07-23 13:20:48 +0530

    Code refactored

--- lib/multi_logger.rb
+++ lib/multi_logger.rb
@@ -6,22 +6,20 @@ module MultiLogger
       name = name.to_s
       rails_logger_class = get_rails_logger_class()
 
-      if rails_logger_class.method_defined?(name)
-        raise "'#{name}' is reserved in #{rails_logger_class} and can not be used as a log accessor name."
-      else
-        logger = Logger.new(*extract_options(name, options))
-        rails_logger_class.class_eval do
-          define_method name.to_sym do
-            logger
-          end
+      raise "'#{name}' is reserved in #{rails_logger_class} and can not be used as a log accessor name." if rails_logger_class.method_defined?(name)
+
+      logger = Logger.new(*extract_options(name, options))
+      rails_logger_class.class_eval do
+        define_method name.to_sym do
+          logger
         end
-        if options[:formatter]
-          logger.formatter = options[:formatter]
-        end
-        logger
       end
+
+      logger.formatter = options[:formatter] if options[:formatter]
     end
 
+    private
+    
     # Computes log file path
     def get_path(name, path=nil)
       if path.nil?
@@ -32,8 +30,7 @@ module MultiLogger
       end
       if !path.end_with?('.log')
         path += '.log'
-      end
-      path
+      end      
     end
 
     def get_rails_logger_class
@@ -47,13 +44,15 @@ module MultiLogger
     end
 
     def extract_options(name, options)
+      path = get_path(name, options[:path])
+
       if options[:shift_age] && options[:shift_size]
-        [get_path(name, options[:path]), options[:shift_age], options[:shift_size]]
+        [path, options[:shift_age], options[:shift_size]]
       elsif options[:shift_age]
         # options[:shift_age] => 'daily', 'weekly'
-        [get_path(name, options[:path]), options[:shift_age]]
+        [path, options[:shift_age]]
       else
-        [get_path(name, options[:path])]
+        [path]
       end
     end
   end
